ff6bb0673e4768436e28086e9dcb9c7be3a68b90
unknown <dungrani786@gmail.com>
2023-07-19 14:20:45 +0530

    Changed filter location into the condition & fixed phpcs error.

--- product-input-fields-for-woocommerce.php
+++ product-input-fields-for-woocommerce.php
@@ -244,7 +244,7 @@ if ( ! class_exists( 'Alg_WC_PIF' ) ) :
 		 * Add tracker completed.
 		 */
 		public static function init_tracker_completed() {
-			$redirect_url = isset( $_SERVER['HTTP_REFERER'] ) ? sanitize_text_field( wp_unslash( $_SERVER['HTTP_REFERER'] ) ) : admin_url( 'admin.php?page=wc-settings&tab=alg_wc_pif' );
+			$redirect_url = isset( $_SERVER['HTTP_REFERER'] ) ? esc_url_raw( wp_unslash( $_SERVER['HTTP_REFERER'] ) ) : admin_url( 'admin.php?page=wc-settings&tab=alg_wc_pif' );
 			header( 'Location: ' . $redirect_url );
 			exit;
 		}

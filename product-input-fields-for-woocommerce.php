10b4b47902b09bc5b55165f44228723c527ac288
gaminikhil <gaminikhil1122@gmail.com>
2025-01-31 16:54:18 +0530

    removed version change from this PR

--- product-input-fields-for-woocommerce.php
+++ product-input-fields-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Product Input Fields for WooCommerce
  * Plugin URI: https://www.tychesoftwares.com/store/premium-plugins/product-input-fields-for-woocommerce/
  * Description: Add custom product input fields to your WooCommerce products. Let customers personalize/customize products effortlessly. Elevate your store experience!
- * Version: 1.12.0
+ * Version: 1.11.0
  * Author: Tyche Softwares
  * Author URI: https://www.tychesoftwares.com/
  * Text Domain: product-input-fields-for-woocommerce
@@ -39,7 +39,7 @@ if (
 
 // Constants.
 if ( ! defined( 'ALG_WC_PIF_VERSION' ) ) {
-	define( 'ALG_WC_PIF_VERSION', '1.12.0' );
+	define( 'ALG_WC_PIF_VERSION', '1.11.0' );
 }
 if ( ! defined( 'ALG_WC_PIF_ID' ) ) {
 	define( 'ALG_WC_PIF_ID', 'alg_wc_pif' );
@@ -84,7 +84,7 @@ if ( ! class_exists( 'Alg_WC_PIF' ) ) :
 		 * @var string Version.
 		 * @access public
 		 */
-		public static $version = '1.12.0';
+		public static $version = '1.11.0';
 
 		/**
 		 * Define an instance for the class.

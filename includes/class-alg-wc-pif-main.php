7122c490d5c7915844a1fc9d6d211bf2c0fd7a54
gaminikhil <gaminikhil1122@gmail.com>
2025-01-31 17:28:28 +0530

    phpcs fixed
    phpcs fixed

--- includes/class-alg-wc-pif-main.php
+++ includes/class-alg-wc-pif-main.php
@@ -520,7 +520,7 @@ if ( ! class_exists( 'Alg_WC_PIF_Main' ) ) {
 						$filename = $value['name'];
 						$validate = wp_check_filetype( $filename ); // Check the file type.
 						if ( ! $validate['type'] ) {
-							die( __( 'File type is not allowed.', 'product-input-fields-for-woocommerce' ) );
+							die( esc_html__( 'File type is not allowed.', 'product-input-fields-for-woocommerce' ) );
 						}
 
 						$name       = $item_id . '_' . $value['name'];
